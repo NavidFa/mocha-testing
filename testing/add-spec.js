e5e5a6387976c5b81d40f274cdc0d3e445977bb1
NavidFa <navid.fallahazad@gmail.com>
2016-09-07 10:29:40 -0600

    first commit

---
+++ testing/add-spec.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const supertest=require('supertest')
+
+const app = require('../app')
+const should = chai.should();
+const api = supertest(app);
+
+
+describe('Sending a post to /api/add',()=>{
+  describe('Should succeed ', ()=>{
+    it('in adding two number together',(done)=>{
+      api.post('/api/add')
+      .send({
+        num1: 5,
+        num2: 2
+      })
+      .expect(200)
+      .end((err,res)=>{
+        if(err) return done(err);
+        res.body.result.should.be.equal(7);
+        done();
+      })
+    })
+    describe('should fail',() =>{
+      it('when the nothing is sent in ', (done)=>{
+        api.post('/api/add')
+        .expect(432)
+        .end((err,res)=>{
+          if(err)return done (err);
+          res.body.message.should.be.equal('no data fool');
+          done();
+        })
+      })
+    })
+  })
+})
---
+++ testing/canary-spec.js
@@ -0,0 +1,18 @@
+const chai = require ('chai');
+const should = chai.should();
+const expect = chai.expect;
+const assert = chai.assert;
+
+describe('Canary test',() => {
+  it('the streing hello should be hello',() => {
+    const hello = 'hello';
+    hello.should.be.equal('hello');
+  })
+  it('typeof string should be string',()  => {
+    const str='qwfp';
+
+    str.should.be.a('string')
+    expect(str).to.be.a('string');
+    assert.typeOf(str,'string');
+  })
+})
